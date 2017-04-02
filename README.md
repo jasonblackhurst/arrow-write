af913253c93f012a3982bc6c659d60f61e4c82be
jason-blackhurst <jason.blackhurst@mheducation.com>
2017-04-01 23:34:44 -0400

    intial checking of arrow-write

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # arrow-write
 a simple node lambda to write an item to dynamodb
+
+# to deploy use
+claudia update
---
+++ arrowWrite.js
@@ -0,0 +1,26 @@
+console.log('Loading event');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
+
+exports.handler = function(event, context) {
+    console.log("Request received:\n", JSON.stringify(event));
+    console.log("Context received:\n", JSON.stringify(context));
+
+    var tableName = "Recipe";
+//    var datetime = new Date().getTime().toString();
+//    var name = "new Entry";
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+docClient.put({
+            "TableName": tableName,
+            "Item": event
+        }, function(err, data) {
+            if (err) {
+                context.fail('ERROR: Dynamo failed: ' + err);
+            } else {
+                console.log('Dynamo Success: ' + JSON.stringify(data, null, '  '));
+                context.succeed('SUCCESS');
+            }
+        });
+
+}
